cdbb9adde7097f12c0f9e273afa62539ecf01268
Gabriel <macigabi4@gmail.com>
2025-01-28 19:36:33 -0300

    First commit

---
+++ class01.php
@@ -0,0 +1,32 @@
+<?php
+
+///// Variáveis
+// $err = (string) "Striker";
+// $name = "Striker";
+// $age = 14;
+
+///// Condicional
+// if ($name == "Striker" && $age == 12){
+//     echo "Chora";
+// } else {
+//     echo "Nenhuma condição foi verdadeira";
+// }
+
+///// Laço de repetição
+// for($i = 0; $i < 3; $i++){
+//     echo "Chora " . $i . "\n";
+// }
+
+///// Switch-case
+// echo "Type a number:\n";
+// $op = readline();
+// switch ($op){
+//     case 1:
+//         echo "Chora";
+//         break;
+//     case 2:
+//         echo "Chorei";
+//         break;
+//     default:
+//         echo "Opção inválida";
+// }
