0109b56744f44ffb811e685afa0fefa4fe01d762
gabriel <71945882+Hikaruya9@users.noreply.github.com>
2025-03-19 22:11:24 -0300

    added project1

---
+++ Project1/painel.php
@@ -0,0 +1,14 @@
+<?php
+
+include 'protect.php';
+
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+?>
+
+<h1> Bem vindo <?= $_SESSION['nome']; ?> </h1>
+<a href="logout.php">Deslogar</a>
+<a href="">Atualizar conta</a>
+<a href="index.php">Excluir conta</a>
